e1e77ed5c35c2d22a07266432ee87ef9aafb3f13
jonapr622 <jonapr622@gmail.com>
2024-10-16 18:24:32 -0600

    actividad 1

---
+++ tipos-de-dato.js
@@ -0,0 +1,34 @@
+console.log("Hola mundo");
+console.log('Hola ' + 'Mundo');
+console.log((5+5.555)/2*10);
+console.log(parseInt("5.5") + parseInt("5.5"));
+console.log(parseFloat("5.555") + parseFloat("5.555"));
+console.log(true);
+console.log(false == 0);
+let numero = 30;
+console.log(numero);
+let numeroPrimo = 11;
+console.log(numeroPrimo);
+let mensaje = "Mensaje secreto";
+console.log(mensaje);
+mensaje = "otro mensaje secreto";
+console.log(mensaje);
+let miVariable;
+console.log(miVariable);
+
+console.log(typeof 42)
+console.log(typeof 'Veinticinco');
+console.log(typeof -666);
+console.log(typeof true);
+console.log(typeof 0);
+console.log(typeof "''");
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof false);
+
+console.log(typeof (5 + 5.555)/2);
+console.log(typeof parseInt("5.5") + parseInt("5.5"));
+console.log(typeof parseFloat("5.555") + parseFloat("5.555"));
+console.log(typeof true);
+console.log(typeof numero);
+console.log(typeof mensaje);
